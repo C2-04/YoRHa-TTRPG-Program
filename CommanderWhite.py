4534125c297737e75afef931a70cc87e3012da3c
C2-04 <pichurdboi@gmail.com>
2024-01-26 09:14:20 -0700

    Added a separate program for battle calculations

--- CommanderWhite.py
+++ CommanderWhite.py
@@ -5,70 +5,6 @@ import random
 import math
 import pickle
 
-def calculateReward(lower_bound, upper_bound):
-    reward = math.floor((upper_bound - lower_bound)/2) + lower_bound
-    for i in range(0, upper_bound - lower_bound):
-        if random.random() >= 0.5:
-            reward = reward + 1
-        else:
-            reward = reward - 1
-    print("Reward:", reward)
-    return(reward)
-
-def placeEnemies(enemies):
-    enemyPlaces = []
-    for i in range(enemies):
-        xCoord = random.randint(3, 10)
-        yCoord = random.choice(["c", "d", "e", "f", "g", "h", "i", "j"])
-        enemyPlaces.append(str(xCoord) + yCoord)
-    print("Enemy places:", ', '.join(enemyPlaces))
-
-def selectEnemies(weight):
-    enemNum = 1
-    chosenEnemies = []
-    enemiesList = {"Small Stubby" : 20,
-                   "Medium Biped" : 50,
-                   "Horseback Machine" : 80,
-                   "Small Flier": 30,
-                   "Medium Flier": 140,
-                   "Explodey Boy": 10,
-                   "Axe Flier": 90,
-                   "Mech Worm": 150,
-                   "Gunslinger": 70,
-                   }
-    selectedEnemy = random.choice(list(enemiesList.keys()))
-    while weight >= enemiesList[selectedEnemy]:
-        selectedWeight = enemiesList[selectedEnemy]
-        if random.random() >= 0.9:
-            selectedWeight = selectedWeight*2
-            selectedEnemy = "Enhanced "+ selectedEnemy
-        if random.random() >= 0.9:
-            selectedWeight = selectedWeight*2
-            selectedEnemy =  selectedEnemy + " with a Gun"
-        weight = weight - selectedWeight
-        chosenEnemies.append(selectedEnemy)
-        enemNum = enemNum + 1
-        selectedEnemy = random.choice(list(enemiesList.keys()))
-    print(enemNum, "enemies:", ', '.join(chosenEnemies), "and a", abs(weight), "layer Defenseless Stack!")
-    return enemNum
-
-def itemRewards(weight):
-    rewardList = []
-    for i in range(weight):
-        itemRNG = random.random()
-        if itemRNG <= 0.67:
-            rewardList.append(random.choice(["Dented Plate", "Broken Circuit", 'Stripped Screw', 'Small Gear',
-                                             "Broken Key", 'Warped Wire', 
-                                             'Rusty Bolt', 'Crushed Nut', 'Dented Socket', 'Severed Cable', 'Broken Battery']))
-        elif itemRNG <= 0.92:
-            rewardList.append(random.choice(["Titanium Alloy", 'Large Gear', 'Stretched Coil', 'Sturdy Socket', "Pristine Cable",
-                                              "Large Battery"]))
-        elif itemRNG <= 0.99:
-            rewardList.append(random.choice(["Memory Alloy", "Machine Arm", "Machine Leg", 'Machine Torso', "Machine Head"]))
-        else:
-            rewardList.append("Machine Core")
-    print(weight, "rewards:", ', '.join(rewardList))
-
 def creatifier():
     unittype = input("Need a specific type? ")
     if unittype == "no":
@@ -166,30 +102,6 @@ def statincreasifier(derstat, stats, desiredUnit):
     else:
         print("Fuck-a-doodle-doo! The stat didn't increase.")
 
-def hitcalcifier(Attacker, Defender):
-    if len(Attacker) <= 3:
-        atksts = getChar(Attacker)
-    else:
-        atksts = getEnemy(Attacker)
-    if len(Defender) <= 3:
-        defsts = getChar(Defender)
-    else:
-        defsts = getEnemy(Defender)
-    atk = atksts[atksts['Weapon Type']] + random.randint(-2, 3)
-    mrl = atksts['MOR']
-    dfs = defsts['Defense']
-    acc = float(input('Enter accuracy: '))
-    evd = defsts['Evasion']
-    if atksts['Unit Type'] == 'B':
-        atk = atk + 3
-    if defsts['Unit Type'] == 'D':
-        dfs = dfs+ 3
-    if random.random() >= acc*(1 - evd):
-        print("You missed.")
-    elif random.randint(1, 100) <= mrl:
-        print("Critical strike for", 2*max(atk-dfs, 1), "damage!")
-    else:
-        print("Hit for", max(atk-dfs, 1), 'damage!')
 def getChar(unitName):
     infile = open(unitName+'.YoRHa', 'rb')
     stats = pickle.load(infile)
@@ -200,23 +112,7 @@ def getEnemy(unitName):
     stats = pickle.load(infile)
     infile.close()
     return(stats)
-def createEnemy(enemName):
-    stats = {}
-    stats['Machine'] = int(input('Enter machine attack: '))
-    stats['Defense'] = int(input('Enter defense: '))
-    stats['Movement'] = int(input('Enter mvement: '))
-    stats['MOR'] = 2
-    stats['Yield'] = int(input('Enter XP Yield: '))
-    stats['Max Health'] = int(input('Enter max health: '))
-    stats['Weapon Type'] = 'Machine'
-    stats['Evasion'] = float(input('Enter evasion: '))
-    stats['Accuracy'] = float(input('Enter accuracy: '))
-    stats['Unit Type'] = 'Machine'
-    stats['Personality'] = 'Machine'
-    with open("enemies/"+enemName+'.N2', 'wb') as outfile:
-        pickle.dump(stats, outfile)
-        outfile.close()
-    
+
 def editChar(desiredUnit, stats, desiredStat, newValue):
     stats[desiredStat] = newValue
     with open(desiredUnit+'.YoRHa', 'wb') as outfile: #WRITE BINARY DUMB FUCK
@@ -239,21 +135,7 @@ def printUnits():
         units = pickle.load(infile)
         infile.close()
     print(units)
-def grantXP(toGrant, desiredUnit):
-    stats = getChar(desiredUnit)
-    startLevel = stats['Level']
-    stats['TotalXP'] = stats['TotalXP'] + toGrant
-    stats['Level'] = int(math.ceil(stats['TotalXP']**(1/3)))
-    if startLevel < stats["Level"]:
-        print(desiredUnit + ' has reached level ' + str(stats['Level']) + '.')
-        for i in range((stats['Level'] - startLevel)*(math.floor(stats['Level']/20) + 1)):
-            levelstat = input('Enter the stat to increase by 1: ')
-            stats[levelstat] = stats[levelstat] + 1
-    stats['Max Health'] = 80 + (stats["Level"] + 4)*stats['TGH']
-    stats['Health'] = stats['Max Health']
-    with open(desiredUnit+'.YoRHa', 'wb') as outfile: #WRITE BINARY DUMB FUCK
-        pickle.dump(stats, outfile)
-    print("Operation complete.")
+
 def listweapons():
     idFile = open('weapons/bases/totalweapons.txt', 'r')
     id = int(idFile.readline().strip())
@@ -277,24 +159,13 @@ def listunits():
 def main():
     mode = None
     while mode != "quit":
-        mode = input("Enter the needed operation (create, fightsetup, calcskill, calchit, getunit, editunit, grantXP, quit, listweap): ")
-        if mode == "fightsetup":
-            lower_bound = int(input("Enter lower reward bound: "))
-            upper_bound = int(input("Enter upper reward bound: "))
-            weight = calculateReward(lower_bound, upper_bound)
-            enemNum = selectEnemies(weight)
-            placeEnemies(enemNum)
-            itemRewards(enemNum)
+        mode = input("Enter the needed operation (create, listweap, lisunit, editunit, register, quit): ")
         if mode == "create":
             creatifier()
         if mode == 'listweap':
             listweapons()
         if mode == 'listunit':
             listunits()
-        if mode == "grantXP":
-            toGrant = int(input("Enter the amount of XP you wish to give: "))
-            desiredUnit = input("Enter the unit to grant XP to: ")
-            grantXP(toGrant, desiredUnit)
         if mode == "editunit":
             desiredUnit = input("Enter the name of the unit you wish to test: ")
             desiredEditUnitStats = getChar(desiredUnit)
@@ -305,16 +176,6 @@ def main():
             registerUnit(input("Enter the name of the unit you wish to register: "))
         if mode == "getunit":
             print(getChar(input("Enter the name of the unit you wish to display: ")))
-        if mode == "createEnemy":
-            createEnemy(input('Enter the name of the enemy you wish to create: '))
-        if mode == "calcskill":
-            desiredUnit = input("Enter the name of the unit you wish to test: ")
-            desiredUnitStats = getChar(desiredUnit)
-            desiredStat = input("Enter the stat you wish to test: ")
-            statincreasifier(desiredStat, desiredUnitStats, desiredUnit)
-        if mode == "calchit":
-            attackingUnit = input("Attacker: ")
-            defendingUnit = input("Defender: ")
-            hitcalcifier(attackingUnit, defendingUnit)
 
+        
 main()
---
+++ InstructorBlack.py
@@ -0,0 +1,113 @@
+from CommanderWhite import *
+import math
+import random
+import pickle
+
+def calculateReward(lower_bound, upper_bound):
+    reward = math.floor((upper_bound - lower_bound)/2) + lower_bound
+    for i in range(0, upper_bound - lower_bound):
+        if random.random() >= 0.5:
+            reward = reward + 1
+        else:
+            reward = reward - 1
+    print("Reward:", reward)
+    return(reward)
+def placeEnemies(enemies):
+    enemyPlaces = []
+    for i in range(enemies):
+        xCoord = random.randint(3, 10)
+        yCoord = random.choice(["c", "d", "e", "f", "g", "h", "i", "j"])
+        enemyPlaces.append(str(xCoord) + yCoord)
+    print("Enemy places:", ', '.join(enemyPlaces))
+
+def selectEnemies(weight):
+    enemNum = 1
+    chosenEnemies = []
+    enemiesList = {"Small Stubby" : 20,
+                   "Medium Biped" : 50,
+                   "Horseback Machine" : 80,
+                   "Small Flier": 30,
+                   "Medium Flier": 140,
+                   "Explodey Boy": 10,
+                   "Axe Flier": 90,
+                   "Mech Worm": 150,
+                   "Gunslinger": 70,
+                   }
+    selectedEnemy = random.choice(list(enemiesList.keys()))
+    while weight >= enemiesList[selectedEnemy]:
+        selectedWeight = enemiesList[selectedEnemy]
+        if random.random() >= 0.9:
+            selectedWeight = selectedWeight*2
+            selectedEnemy = "Enhanced "+ selectedEnemy
+        if random.random() >= 0.9:
+            selectedWeight = selectedWeight*2
+            selectedEnemy =  selectedEnemy + " with a Gun"
+        weight = weight - selectedWeight
+        chosenEnemies.append(selectedEnemy)
+        enemNum = enemNum + 1
+        selectedEnemy = random.choice(list(enemiesList.keys()))
+    print(enemNum, "enemies:", ', '.join(chosenEnemies), "and a", abs(weight), "layer Defenseless Stack!")
+    return enemNum
+
+def itemRewards(weight):
+    rewardList = []
+    for i in range(weight):
+        itemRNG = random.random()
+        if itemRNG <= 0.67:
+            rewardList.append(random.choice(["Dented Plate", "Broken Circuit", 'Stripped Screw', 'Small Gear',
+                                             "Broken Key", 'Warped Wire', 
+                                             'Rusty Bolt', 'Crushed Nut', 'Dented Socket', 'Severed Cable', 'Broken Battery']))
+        elif itemRNG <= 0.92:
+            rewardList.append(random.choice(["Titanium Alloy", 'Large Gear', 'Stretched Coil', 'Sturdy Socket', "Pristine Cable",
+                                              "Large Battery"]))
+        elif itemRNG <= 0.99:
+            rewardList.append(random.choice(["Memory Alloy", "Machine Arm", "Machine Leg", 'Machine Torso', "Machine Head"]))
+        else:
+            rewardList.append("Machine Core")
+    print(weight, "rewards:", ', '.join(rewardList))
+
+def hitcalcifier(Attacker, Defender):
+    if len(Attacker) <= 3:
+        atksts = getChar(Attacker)
+    else:
+        atksts = getEnemy(Attacker)
+    if len(Defender) <= 3:
+        defsts = getChar(Defender)
+    else:
+        defsts = getEnemy(Defender)
+    atk = atksts[atksts['Weapon Type']] + random.randint(-2, 3)
+    mrl = atksts['MOR']
+    dfs = defsts['Defense']
+    acc = float(input('Enter accuracy: '))
+    evd = defsts['Evasion']
+    if atksts['Unit Type'] == 'B':
+        atk = atk + 3
+    if defsts['Unit Type'] == 'D':
+        dfs = dfs+ 3
+    if random.random() >= acc*(1 - evd):
+        print("You missed.")
+    elif random.randint(1, 100) <= mrl:
+        print("Critical strike for", 2*max(atk-dfs, 1), "damage!")
+    else:
+        print("Hit for", max(atk-dfs, 1), 'damage!')
+
+def main():
+    mode = None
+    while mode != "quit":
+        mode = input("Enter the needed operation (fightsetup, calchit, quit): ")
+        if mode == "fightsetup":
+            lower_bound = int(input("Enter lower reward bound: "))
+            upper_bound = int(input("Enter upper reward bound: "))
+            weight = calculateReward(lower_bound, upper_bound)
+            enemNum = selectEnemies(weight)
+            placeEnemies(enemNum)
+            itemRewards(enemNum)
+        if mode == "calchit":
+            attackingUnit = input("Attacker: ")
+            defendingUnit = input("Defender: ")
+            hitcalcifier(attackingUnit, defendingUnit)
+        if mode == "calcskill":
+            desiredUnit = input("Enter the name of the unit you wish to test: ")
+            desiredUnitStats = getChar(desiredUnit)
+            desiredStat = input("Enter the stat you wish to test: ")
+            statincreasifier(desiredStat, desiredUnitStats, desiredUnit)
